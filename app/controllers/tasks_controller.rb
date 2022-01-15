23ba51e6da648914aa09be9bfcd2f0b169304782
Bastien Genin <genin440@gmail.com>
2022-01-15 11:46:46 +0100

    Add validation on Task model

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,17 @@
 class TasksController < ApplicationController
+
+  before_action :set_taches
+
+  def index
+    @taches = ???
+  end
+
+  def list
+  end
+
+  private
+
+  def set_taches
+    @taches =
+  end
 end
